5a6701daaf4144cc10af63d3b36d4314b1c74e62
ThakareSakshi <sakshithakare121@gmail.com>
2023-10-09 13:10:34 +0530

    uploaded

---
+++ script.js
@@ -0,0 +1,24 @@
+let textInput=document.getElementById("text-input");
+
+let characterCount=document.getElementById("character-count");
+let  remainingCount=document.getElementById("remaining-count");
+
+textInput.addEventListener("keyup",(e)=>{
+    let count=textInput.value.length;
+    if(count>=100){
+       
+        document.querySelector("section").classList.add("borderC");
+        document.querySelector("h2").classList.add("borderC");
+        document.querySelectorAll(".detail").classList.add("borderC");
+       
+      
+    }else{
+        document.querySelector("section").classList.remove("borderC");
+        document.querySelector("h2").classList.remove("borderC");
+        document.querySelector(".detail").classList.remove("borderC");
+    }
+    characterCount.innerText=count;
+    remainingCount.innerText=100-count;
+    console.log(document.getElementById("text-input").value.length);
+   
+})
